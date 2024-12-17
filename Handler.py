e0f5666d23af7d1bae331b5b54d788455e4fa2fe
Noelle Zahara <noellezahara@gmail.com>
2024-12-17 14:08:06 -0700

    Add concrete request handlers

--- Handler.py
+++ Handler.py
@@ -50,3 +50,38 @@ class Handler(IHandler):
     @abstractmethod
     def process_request(self, request: Request):
         pass
+
+
+class TeamLeader(Handler):
+    def process_request(self, request: Request):
+        if not self.check_constraints(request):
+            self._success = False
+            return
+        if request.days <= 7:
+            self._success = True
+        else:
+            print("Team Leader cannot handle this request")
+
+
+class ProjectLeader(Handler):
+    def process_request(self, request: Request):
+        if request.days <= 14:
+            self._success = True
+            self.check_constraints(request)
+        else:
+            print("Project Leader cannot handle this request")
+
+
+class HumanResources(Handler):
+    def process_request(self, request: Request):
+        if request.days <= 21:
+            self._success = True
+            self.check_constraints(request)
+        else:
+            raise ValueError("HR cannot handle this request")
+
+
+class Manager(Handler):
+    def process_request(self, request: Request):
+        self._success = True
+        self.check_constraints(request)
